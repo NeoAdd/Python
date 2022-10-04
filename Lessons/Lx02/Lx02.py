aa3ee8f2b06063193b29fb471643dde7fcf11b00
Alex <Lesnik290199@gmail.com>
2022-10-04 17:16:06 +0500

    домашнее задание семинара 2  лек

--- Lessons/Lx02/Lx02.py
+++ Lessons/Lx02/Lx02.py
@@ -1,6 +1,27 @@
-s = 'hello world'
+from turtle import color
+from unittest.mock import patch
+
+
+s = 'Работа с файлами читать, записывать, добавление'
 
 print(s) # вывод строки
 
+colors = ['red', 'green', 'blue']
+data = open('file.txt', 'a')  
+# a- открытие для добавление данных
+# r- открытие для чтение данных
+# w- открытие для записи данных
+
+#data.writelines(colors) # разделителя не будет
+data.write('\nLINE 12\n')
+data.write('\nLINE 13\n')
+data.close()
+
+exit()
+patch = 'file.txt'
+data = open(path, 'r')
+for line in data:
+    print(line)
+data.close()
 
 
---
+++ Lessons/Lx02/Lx0222.py
@@ -0,0 +1,11 @@
+import Lx022  #обращаемся к файлу Lx022.py
+
+print(Lx022.f(1))
+
+
+import Lx022 as Lx  #обращаемся к файлу Lx022.py сокращенно
+
+print(Lx.f(1))
+
+
+

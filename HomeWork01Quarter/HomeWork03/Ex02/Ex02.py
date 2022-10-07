3420379a0fb38ce6d181b6f519e47c56f38e26da
Alex <Lesnik290199@gmail.com>
2022-10-07 21:35:26 +0500

    домашнее задание семинара 3

---
+++ HomeWork01Quarter/HomeWork03/Ex02/Ex02.py
@@ -0,0 +1,14 @@
+# первый и последний
+
+from xml.dom import xmlbuilder
+
+
+a = [2, 3, 4, 5, 6]
+ans = a[::len(a)-1]
+
+
+print (" avdsav :" + str(ans))
+
+
+
+
---
+++ HomeWork01Quarter/HomeWork03/Ex03/Ex03.py
@@ -0,0 +1,21 @@
+# Напишите программу, которая найдёт 
+# произведение пар чисел списка.
+#  Парой считаем первый и последний 
+# элемент, второй и предпоследний и т.д.
+
+
+list = [2, 3, 6, 4, 3, 5]
+
+# if len(list)%2 == 0:
+#     size = len(list) // 2
+import math 
+size = math.ceil(len(list)/2)
+print(size)
+list2 = []
+for i in range(size):
+    list2.append(list[i]*list[-i - 1])
+print(list2)
+
+
+
+

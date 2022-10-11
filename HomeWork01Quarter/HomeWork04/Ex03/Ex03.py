585abfb5834bddfac396fb5a279f7547678f0a0b
Alex <Lesnik290199@gmail.com>
2022-10-11 15:00:25 +0500

    решил  2 задачи  ДЗ по семинару 4

--- HomeWork01Quarter/HomeWork04/Ex03/Ex03.py
+++ HomeWork01Quarter/HomeWork04/Ex03/Ex03.py
@@ -12,7 +12,7 @@ def fill_number_list(n=20, min=10, max=99) -> list:
         number_list.append(random.randint(min, max)) 
     return number_list
 
-def unique_values_list(user_list) -> list:
+def filt_values_list(user_list) -> list:
     new_list = [user_list[0]]
     for i in range(1, len(user_list)):
         for j in range(len(new_list)):
@@ -22,7 +22,7 @@ def unique_values_list(user_list) -> list:
                 new_list.append(user_list[i])
     return new_list
 
-source_list = fill_number_list(20, 10, 50)
-unique_list = unique_values_list(source_list)
-print(f'Первый список {source_list} -> отфильтрованные значения (без дубликата)')
-print(unique_list)
+first_list = fill_number_list(20, 10, 50)
+filt_list = filt_values_list(first_list)
+print(f'Первый список {first_list} -> отфильтрованные значения (без дубликата)')
+print(filt_list)

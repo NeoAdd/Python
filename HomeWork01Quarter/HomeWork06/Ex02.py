76fa1204afdda8dd75fe8d348e174feef16857ce
Alex <Lesnik290199@gmail.com>
2022-10-21 20:14:24 +0500

    дз семинара 6

--- HomeWork01Quarter/HomeWork06/Ex02.py
+++ HomeWork01Quarter/HomeWork06/Ex02.py
@@ -4,4 +4,13 @@
 # приоритет операций стандартный
 
 
-
+list = input('Введите выражение: ')
+print(list)
+result = 0
+if list.find('+') != -1:
+    list = list.split("+")   # 2+3*9
+    result = int(list[0]) + int(list[1])
+elif list.find('-') != -1:
+    list = list.split("-")   # 2+3*9
+    result = int(list[0]) - int(list[1])
+print(result)
